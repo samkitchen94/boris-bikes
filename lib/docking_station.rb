df03c16cfcba16c24fb830db90f8b0ad58635a3c
Sam <samkitchen94@gmail.com>
2019-05-02 17:36:47 +0100

    ch 14, error added if docking station is full and stores bikes as an array

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
-
+  def initialize
+    @bike_collection = []
+  end
   def release_bike
-    fail "No bikes available" unless @bike
-    @bike
+    fail "No bikes available" if @bike_collection.empty?
+    @bike_collection.pop
   end
 
   def dock(bike)
-    fail "Dock is full" if @bike
-    @bike = bike
+    fail "Dock is full" if @bike_collection.count >= 20
+    @bike_collection << bike
+    bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'expects DockingStation to respond to release bike method' do
-    expect(subject).to respond_to :release_bike
-  end
-
   describe '#release_bike' do
     it "releases a bike" do
       bike = Bike.new
@@ -14,29 +10,27 @@ describe DockingStation do
     it "raises an error when there are no bikes" do
       expect {subject.release_bike}.to raise_error "No bikes available"
     end
+    it 'expects DockingStation to respond to release bike method' do
+      expect(subject).to respond_to :release_bike
+    end
   end
 
-  it "allows user to dock bike" do
-    expect(subject).to respond_to(:dock).with(1).argument
-  end
-
-  it "is expected to respond to docking station" do
-    expect(subject).to respond_to :bike
-  end
-  describe '#dock(bike)' do
+  describe '#dock' do
     it "docks a bike" do
       bike = Bike.new
       expect(subject.dock(bike)).to eq(bike)
     end
     it "raises an error if dock is full" do
-      subject.dock(Bike.new)
-      expect {subject.dock(Bike.new)}.to raise_error "Dock is full"
+      20.times { subject.dock Bike.new }
+      expect {subject.dock Bike.new}.to raise_error "Dock is full"
     end
-  end
-
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+    it "returns docked bikes" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.dock(bike)).to eq(bike)
+    end
+    it "allows user to dock bike" do
+      expect(subject).to respond_to(:dock).with(1).argument
+    end
   end
 end

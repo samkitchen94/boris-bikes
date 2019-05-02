abe322c2f9f065f3bfbdf42dc5e8475dd7834b76
Sam <samkitchen94@gmail.com>
2019-05-02 14:42:21 +0100

    ch 11 instance variables added to store docked bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,4 +9,23 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it "allows user to dock bike" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "is expected to respond to docking station" do
+    expect(subject).to respond_to :bike
+  end
+
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
+  end
 end

c634d4c7f4447151e010f6df5370d444bb8ebb1e
Sam <samkitchen94@gmail.com>
2019-05-02 15:11:16 +0100

    ch 12 completed, gives error message for when there are no bikes available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,16 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
-  it "releases a working bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
+
+    it "raises an error when there are no bikes" do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
   end
 
   it "allows user to dock bike" do

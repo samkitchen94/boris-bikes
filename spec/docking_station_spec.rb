08e713637468cab2783d6159b7ee242e01aefcb7
Sam <samkitchen94@gmail.com>
2019-05-02 14:16:04 +0100

    ch 10 completed, docking stations can get bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
   it 'expects DockingStation to respond to release bike method' do
     expect(subject).to respond_to :release_bike
   end
+
+  it "releases a working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
